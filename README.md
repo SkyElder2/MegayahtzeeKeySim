a8e55067b997c14d53b1249176803df0975c1497
Waeland Elder <wselder@uark.edu>
2024-04-23 10:53:46 -0500

    Init Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Megayahtzee Keyboard Play Script
+
+To resolve dependencies, run: 'pip install -r /path/to/requirements.txt'
+
+To start the program, run autoscript.py.
+To pause and unpause the program, use '|' (starts paused)
+
+You should be able to use the default config if playing on a 1080p screen.
+If your screen size is different or you're not playing in full-screen, you can press 'q' to create a new config.
+After starting the config, click on all seven dice from top-left to bottom-right, then click the roll button, and then click all of the score selection locations excluding the bonus.
+Also, the currently selected score tab resets after each choice, so that it is easier to keep track of after each roll.
+Refer to the program console for any further instructions.
---
+++ autoscript.py
@@ -0,0 +1,137 @@
+import time
+import functools
+from pynput.mouse import Button
+from pynput.mouse import Controller as mController
+from pynput.mouse import Listener as mListener
+from pynput.keyboard import Listener, KeyCode, Key
+from pynput.keyboard import Controller as kController
+
+print = functools.partial(print, flush=True)
+
+
+keyboard = kController()
+mouse = mController()
+
+# Globals
+cfg = 'config.txt'
+pauseResumeKey = KeyCode(char='|')
+endKey = KeyCode(char='?')
+calibrateKey = KeyCode(char='q')
+keybindsKey = KeyCode(char='+')
+helpKey = KeyCode(char='j')
+rollKey = Key.space
+tab = Key.tab
+pause = True
+running = True
+calibrating = False
+scoreTab = 0
+scoreCats = 5
+catNums = [7, 5, 5, 7, 10]
+catNames = ["Basic Tallies", "Common Combos", "Wildcard Scores", "Complex Clusters", "Super Special Rolls"]
+NUMSCORETYPES = sum(catNums)
+
+# Keybinds
+diceKeys = [KeyCode(char='t'), KeyCode(char='y'), KeyCode(char='f'), KeyCode(char='g'), KeyCode(char='h'), KeyCode(char='v'), KeyCode(char='b')]
+clickLoc = []
+
+def mouseClick(x, y, button, pressed):
+        global calibrating
+        if calibrating and pressed:
+            x, y = mouse.position
+            clickLoc.append((x, y))
+            if len(clickLoc) == len(diceKeys) + 1 + NUMSCORETYPES:
+                mouseListener.stop()
+                # Save data to file for loading next time
+                file = open(cfg, 'w')
+                for i in range(len(clickLoc)):
+                    setting = str(clickLoc[i]) + "\n"
+                    file.write(setting)
+
+def keybinds():
+    return
+
+def keyPress(key):
+    global pause, calibrating, clickLoc, scoreTab
+    if key == pauseResumeKey:
+        pause = not pause
+    elif key == helpKey:
+        print("'q' - Further info. After pressing q, click dice from top left to bottom right, then the roll button, \nand then the selection boxes from top left to bottom right, then press q again.")
+        print("\nTo reiterate, the '|' can be used to pause the program so that typing elsewhere does not cause the mouse to jump around.")
+        print("Calibration only needs to be done once, as the inputs will be saved and loaded if present the next time the program is run.")
+        print("If you are using the default inputs provided, you just need to unpause to begin using the program.")
+        print("A side note, the program does not need focus to work.")
+        print("Feel free to DM or message in the programming channel to give feedback and I'll do my best to implement it.")
+        print("Also, if you notice any bugs, let me know. Enjoy!")
+    elif key == endKey:
+        mouseListener.stop()
+        kListener.stop()
+    elif (not pause):
+        if key == keybindsKey:
+            keybinds()
+        elif key == calibrateKey:
+            calibrating = not calibrating
+            if calibrating:
+                clickLoc = []
+        else:
+            processed = False
+            for i in range(len(diceKeys)):
+                if key == diceKeys[i]:
+                    processed = True
+                    mouse.position = clickLoc[i]
+                    mouse.click(Button.left, 1)
+            if key == rollKey:
+                mouse.position = clickLoc[7]
+                mouse.click(Button.left, 1)  
+            elif key == tab:
+                scoreTab = (scoreTab + 1) % scoreCats
+                print(f"\nCurrent Tab: {catNames[scoreTab]}")
+            elif not processed:
+                for i in range(1, catNums[scoreTab]+1):
+                    if key == KeyCode(char=str(i)) or (catNums[scoreTab] == 10 and key == KeyCode(char='0')):
+                        if i == 0:
+                            val = 10
+                        else:
+                            val = i
+                        if val < catNums[scoreTab] + 1:
+                            j = scoreTab - 1
+                            scoreChoice = 8
+                            while(j > -1):
+                                scoreChoice += catNums[j]
+                                j -= 1
+                            scoreChoice += i - 1
+                            mouse.position = clickLoc[scoreChoice]
+                            mouse.click(Button.left, 1)
+                            scoreTab = 0
+                            print(f"\nCurrent Tab: {catNames[scoreTab]}")
+            
+
+# Load config if present
+try:
+    data = open(cfg, 'r')
+    if data:
+        print("Successfully Opened Config")
+        lines = data.readlines()
+        for line in lines:
+            vals = line.replace("(", "").replace(")", "").split(', ')
+            clickLoc.append((int(vals[0]), int(vals[1])))
+        print("Successfully Loaded Config")
+except IOError:
+    print("No config file detected.\n")
+
+
+print("To use this program, you can either use the saved calibrations or calibrate yourself.")
+print("If playing on a 1080p screen, the saved calibrations should be good.")
+print("CONTROLS")
+print(f"{'|'.ljust(19)} - Pause/Unpause Listeners, starting state Paused.")
+print(f"{'q'.ljust(19)} - Use when unpaused to calibrate locations.")
+print(f"{'t, y, f, g, h, v, b'.ljust(19)} - Dice keys, arranged in the structure of the dice, toggles selection.")
+print(f"{'Spacebar'.ljust(19)} - Rolls dice.")
+print(f"{'j'.ljust(19)} - Help (Just further instructions)")
+print(f"{'?'.ljust(19)} - Exit Program")
+
+print("Current Tab: Basic Tallies")
+
+with Listener(on_press=keyPress) as kListener, \
+    mListener(on_click=mouseClick) as mouseListener:
+    kListener.join()
+    mouseListener.join()

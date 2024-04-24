7d4f298467c5db1f01d5767af151cf0299323af7
Waeland Elder <wselder@uark.edu>
2024-04-24 15:22:24 -0500

    Print Controls, Sort, and Refresh

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ To pause and unpause the program, use '|' (starts paused)
 
 You should be able to use the default config if playing on a 1080p screen.
 If your screen size is different or you're not playing in full-screen, you can press 'q' to create a new config.
-After starting the config, click on all seven dice from top-left to bottom-right, then click the roll button, and then click all of the score selection locations excluding the bonus.
+After starting the config, click on all seven dice from top-left to bottom-right, then click the roll and sort buttons (in that order), and then click all of the score selection locations excluding the bonus. 
+Finally, click the browser refresh page and press 'q' again to save the config. 
+This only needs to be done once per computer assuming you keep the browser in the same location.
 Also, the currently selected score tab resets after each choice, so that it is easier to keep track of after each roll.
 Refer to the program console for any further instructions.
--- autoscript.py
+++ autoscript.py
@@ -20,6 +20,9 @@ calibrateKey = KeyCode(char='q')
 keybindsKey = KeyCode(char='+')
 helpKey = KeyCode(char='j')
 rollKey = Key.space
+refreshKey = KeyCode(char='a')
+reprintKey = KeyCode(char='d')
+sortKey = KeyCode(char='s')
 tab = Key.tab
 pause = True
 running = True
@@ -39,7 +42,7 @@ def mouseClick(x, y, button, pressed):
         if calibrating and pressed:
             x, y = mouse.position
             clickLoc.append((x, y))
-            if len(clickLoc) == len(diceKeys) + 1 + NUMSCORETYPES:
+            if len(clickLoc) == len(diceKeys) + 3 + NUMSCORETYPES:
                 mouseListener.stop()
                 # Save data to file for loading next time
                 file = open(cfg, 'w')
@@ -55,13 +58,15 @@ def keyPress(key):
     if key == pauseResumeKey:
         pause = not pause
     elif key == helpKey:
-        print("'q' - Further info. After pressing q, click dice from top left to bottom right, then the roll button, \nand then the selection boxes from top left to bottom right, then press q again.")
+        print("'q' - Further info. After pressing q, click dice from top left to bottom right, then the roll button, then the sort button, \nand then the selection boxes from top left to bottom right, and then the browser refresh, \nand then press q again.")
         print("\nTo reiterate, the '|' can be used to pause the program so that typing elsewhere does not cause the mouse to jump around.")
         print("Calibration only needs to be done once, as the inputs will be saved and loaded if present the next time the program is run.")
         print("If you are using the default inputs provided, you just need to unpause to begin using the program.")
         print("A side note, the program does not need focus to work.")
         print("Feel free to DM or message in the programming channel to give feedback and I'll do my best to implement it.")
         print("Also, if you notice any bugs, let me know. Enjoy!")
+    elif key == reprintKey:
+        printInstructions()
     elif key == endKey:
         mouseListener.stop()
         kListener.stop()
@@ -72,6 +77,12 @@ def keyPress(key):
             calibrating = not calibrating
             if calibrating:
                 clickLoc = []
+        elif key == refreshKey:
+            mouse.position = clickLoc[-1]
+            mouse.click(Button.left, 1)
+        elif key == sortKey:
+            mouse.position = clickLoc[8]
+            mouse.click(Button.left, 1)
         else:
             processed = False
             for i in range(len(diceKeys)):
@@ -87,14 +98,14 @@ def keyPress(key):
                 print(f"\nCurrent Tab: {catNames[scoreTab]}")
             elif not processed:
                 for i in range(1, catNums[scoreTab]+1):
-                    if key == KeyCode(char=str(i)) or (catNums[scoreTab] == 10 and key == KeyCode(char='0')):
+                    if key == KeyCode(char=str(i)) or (catNums[scoreTab] == 10 and i == 10 and key == KeyCode(char='0')):
                         if i == 0:
                             val = 10
                         else:
                             val = i
                         if val < catNums[scoreTab] + 1:
                             j = scoreTab - 1
-                            scoreChoice = 8
+                            scoreChoice = 9
                             while(j > -1):
                                 scoreChoice += catNums[j]
                                 j -= 1
@@ -118,20 +129,25 @@ try:
 except IOError:
     print("No config file detected.\n")
 
+def printInstructions():
+    print("CONTROLS")
+    print(f"{'|'.ljust(19)} - Pause/Unpause Listeners, starting state Paused.")
+    print(f"{'d'.ljust(19)} - Print Controls Again.")
+    print(f"{'q'.ljust(19)} - Use when unpaused to calibrate locations.")
+    print(f"{'t, y, f, g, h, v, b'.ljust(19)} - Dice keys, arranged in the structure of the dice, toggles selection.")
+    print(f"{'Tab'.ljust(19)} - Switch currently selected score category.")
+    print(f"{'1-9, 0'.ljust(19)} - Select score in current category.")
+    print(f"{'Spacebar'.ljust(19)} - Rolls dice.")
+    print(f"{'s'.ljust(19)} - Sort Dice.")
+    print(f"{'a'.ljust(19)} - Refresh page to restart game.")
+    print(f"{'j'.ljust(19)} - Help (Just further instructions)")
+    print(f"{'?'.ljust(19)} - Exit Program")
 
 print("To use this program, you can either use the saved calibrations or calibrate yourself.")
-print("If playing on a 1080p screen, the saved calibrations should be good.")
-print("CONTROLS")
-print(f"{'|'.ljust(19)} - Pause/Unpause Listeners, starting state Paused.")
-print(f"{'q'.ljust(19)} - Use when unpaused to calibrate locations.")
-print(f"{'t, y, f, g, h, v, b'.ljust(19)} - Dice keys, arranged in the structure of the dice, toggles selection.")
-print(f"{'Tab'.ljust(19)} - Switch currently selected score category.")
-print(f"{'1-9, 0'.ljust(19)} - Select score in current category.")
-print(f"{'Spacebar'.ljust(19)} - Rolls dice.")
-print(f"{'j'.ljust(19)} - Help (Just further instructions)")
-print(f"{'?'.ljust(19)} - Exit Program")
-
-print("Current Tab: Basic Tallies")
+print("If playing on a 1080p screen, the saved calibrations should be good, although it might not.")
+print("Feel free to dm me or message me if you have any questions or suggestions.")
+printInstructions()
+print(f"Current Tab: {catNames[scoreTab]}")
 
 with Listener(on_press=keyPress) as kListener, \
     mListener(on_click=mouseClick) as mouseListener:
